bb76c116ed79bc619cb7d25d8433efc5df296849
zacksailor <zakarabenjawed@gmail.com>
2022-12-16 20:19:31 +0100

    base

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-const puppeteer = require("puppeteer")
-const fs = require("fs/promises")
+const app = require("express")();
 
-async function start() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto("https://learnwebcode.github.io/practice-requests/")
+let chrome = {};
+let puppeteer;
 
-  const names = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
-  })
-  await fs.writeFile("names.txt", names.join("\r\n"))
-
-  await page.click("#clickme")
-  const clickedData = await page.$eval("#data", el => el.textContent)
-  console.log(clickedData)
-
-  const photos = await page.$$eval("img", imgs => {
-    return imgs.map(x => x.src)
-  })
-
-  await page.type("#ourfield", "blue")
-  await Promise.all([page.click("#ourform button"), page.waitForNavigation()])
-  const info = await page.$eval("#message", el => el.textContent)
-
-  console.log(info)
-
-  for (const photo of photos) {
-    const imagepage = await page.goto(photo)
-    await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
-  }
-
-  await browser.close()
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
 }
 
-start()
+app.get("/api", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    res.send(await page.title());
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
+
+module.exports = app;
